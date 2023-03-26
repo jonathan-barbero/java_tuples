46d81819bea27aa1f2426e3cb87bb7db1fa5bd91
jonathan-barbero <jonathan.barbero@gmail.com>
2023-03-26 18:22:09 -0300

    Solves #2 .

--- src/main/java/ar/com/job/java_tuples/comparator/TP1Comparator.java
+++ src/main/java/ar/com/job/java_tuples/comparator/TP1Comparator.java
@@ -8,7 +8,7 @@ public class TP1Comparator<C1 extends Comparable<C1>> implements Comparator<TP1<
     public static <C1 extends Comparable<C1>> int compareTP1(TP1<C1> o1, TP1<C1> o2) {
         if (null == o1 || null == o2) throw new NullPointerException("Unsupported comparation to null for TP1");
         if (o1 == o2) return 0;
-        return null != o1.f1 ? o1.f1.compareTo(o2.f1) : o2.f1.compareTo(o1.f1);
+        return o1.f1.compareTo(o2.f1);
     }
 
     @Override
